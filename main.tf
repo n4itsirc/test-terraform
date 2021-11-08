e4553b54d4e4baeed7114b7195d713831603fcc4
silvacris <csilva@gobetterfly.com>
2021-11-08 17:27:05 -0300

    estructura base

--- main.tf
+++ main.tf
@@ -1,35 +1,7 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.27"
-    }
-  }
-
-  required_version = ">= 0.14.9"
-}
-
-provider "aws" {
-  profile = "default"
-  region  = "us-east-1"
-}
-
-variable "instance_name_var" {
-  description = "Value of the Name tag for the EC2 instance"
-  type        = string
-  default     = "ExampleInstanceVarName"
-}
-
 resource "aws_instance" "app_server" {
   ami           = "ami-02e136e904f3da870"
   instance_type = "t2.micro"
-
   tags = {
     Name = var.instance_name_var
   }
-}
-
-output "instance_public_ip" {
-  description = "Public IP address of the EC2 instance"
-  value       = aws_instance.app_server.public_ip
 }
---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "instance_name_var" {
+  description = "Value of the Name tag for the EC2 instance"
+  type        = string
+}
